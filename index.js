9f96d718b01c79cdf2c52b11693f296d4da0f35f
ash-codiology <ashfachohan7@gmail.com>
2024-07-10 21:19:22 +0500

    push my opp-university-managmenet to github

--- index.js
+++ index.js
@@ -1,33 +1,109 @@
 #! /usr/bin/env node
-import inquirer from "inquirer";
-async function getTargetDate() {
-    const response = await inquirer.prompt([
-        {
-            name: "targetDate",
-            type: "datetime",
-            message: "CHOOSE YOUR TARGET DATE AND TIME 'give your input like this'(yyyy-mm-dd)",
-            format: ["yyyy", "-", "mm", "-", "dd", "", "hh", ":", "MM"],
-            initial: new Date(),
-        },
-    ]);
-    const targetDateString = response.targetDate.toString();
-    return new Date(targetDateString);
-}
-async function startCountdown() {
-    const targetDate = await getTargetDate();
-    function calculateTimeRemaining(targetDate) {
-        const now = new Date();
-        const difference = targetDate.getTime() - now.getTime();
-        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-        return { days, hours, minutes, seconds };
+class Person {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
-    function updateCountdown() {
-        const { days, hours, minutes, seconds } = calculateTimeRemaining(targetDate);
-        console.log(`${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds left`);
+    tostring() {
+        return `Person {name: ${this.name}, age: ${this.age}}`;
     }
-    setInterval(updateCountdown, 1000);
 }
-startCountdown();
+class Student extends Person {
+    rollno;
+    courses = [];
+    constructor(name, age, rollno) {
+        super(name, age);
+        this.rollno = rollno;
+    }
+    registerForCourse(course) {
+        this.courses.push(course);
+        course.AddStudent(this);
+    }
+    tostring() {
+        return ` Student {name: ${this.name}, age: ${this.age}, rollno: ${this.rollno}, courses: [${this.courses.map(course => course.name).join(",")}}`;
+    }
+}
+class Instructor extends Person {
+    salary;
+    courses = [];
+    constructor(name, age, salary) {
+        super(name, age);
+        this.salary = salary;
+    }
+    assignCourse(course) {
+        this.courses.push(course);
+        course.setInstructor(this);
+    }
+    tostring() {
+        return `Instructor {name: ${this.name}, age: ${this.age}, salary: ${this.salary}, courses: [${this.courses.map(course => course.name).join(",")}]}`;
+    }
+}
+class Course {
+    id;
+    name;
+    student = [];
+    instructors = null;
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    AddStudent(student) {
+        this.student.push(student);
+    }
+    setInstructor(instructor) {
+        this.instructors = instructor;
+    }
+    tostring() {
+        return `Course {id: ${this.id}, name: ${this.name}, student: [${this.student.map(student => student.name).join(",")}], instructor: ${this.instructors?.name ?? "none"}}`;
+    }
+}
+class Department {
+    name;
+    courses = [];
+    constructor(name) {
+        this.name = name;
+    }
+    AddCourse(course) {
+        this.courses.push(course);
+    }
+    tostring() {
+        return `Department {name: ${this.name}, courses: [${this.courses.map(course => course.name).join(",")}]}`;
+    }
+}
+const student1 = new Student("aisha", 18, 1000);
+const student2 = new Student("Ahmed", 30, 1001);
+const student3 = new Student("fatima", 26, 1002);
+const instructor1 = new Instructor("Hamzah", 23, 5000000);
+const instructor2 = new Instructor("Asharib", 22, 3000000);
+const instructor3 = new Instructor("Okasha", 20, 1000000);
+const course1 = new Course(1, "Artifical Intelligence");
+const course2 = new Course(2, "Metaverse");
+const course3 = new Course(3, "Blockchain");
+student1.registerForCourse(course1);
+student2.registerForCourse(course2);
+student3.registerForCourse(course3);
+instructor1.assignCourse(course1);
+instructor2.assignCourse(course2);
+instructor1.assignCourse(course3);
+const department1 = new Department("Department of Computer Science");
+const department2 = new Department("Department of Digital Media");
+const department3 = new Department("Department of Information System");
+department1.AddCourse(course1);
+department2.AddCourse(course2);
+department3.AddCourse(course3);
+console.log(department1.courses[0].tostring());
+console.log(instructor1.courses.map(course => course.tostring()));
+console.log(student1.courses[0].tostring());
+console.log(course3.student.map(student => student.tostring()));
+console.log(student1.courses.map(course => course.tostring()));
+console.log("All Instructors");
+console.log(instructor1.tostring());
+console.log(instructor2.tostring());
+console.log(instructor3.tostring());
+console.log("All Students");
+console.log(student1.tostring());
+console.log(student2.tostring());
+console.log(student3.tostring());
+export {};
--- index.ts
+++ index.ts
@@ -1,41 +1,129 @@
 #! /usr/bin/env node
-import inquirer from "inquirer";
+class Person{
+  name : string;
+  age: number;
 
-async function getTargetDate(): Promise<Date> {
-       const response = await inquirer.prompt(
-    [
-        {
-            name: "targetDate",
-            type: "datetime",
-            message: "CHOOSE YOUR TARGET DATE AND TIME 'give your input like this'(yyyy-mm-dd)",  
-            format: ["yyyy", "-", "mm", "-", "dd", "","hh", ":", "MM"],
-            initial: new Date(),
-        },
-    ]
-);
-      const targetDateString = response.targetDate.toString();
-    return new Date(targetDateString);
- }
+  constructor(name:string,age:number){
+   this.name = name;
+   this.age = age;
+  }
+  tostring():string{
+      return `Person {name: ${this.name}, age: ${this.age}}`
+  }
+}
 
-async function startCountdown() {
-      const targetDate: Date = await getTargetDate();
+class Student extends Person{
+  rollno:number;
+  courses:Course[] = [];
 
-    function calculateTimeRemaining(targetDate: Date): { days: number; hours: number; minutes: number; seconds: number } {
-      const now: Date = new Date();
-      const difference: number = targetDate.getTime() - now.getTime();
-      const days: number = Math.floor(difference / (1000 * 60 * 60 * 24));
-      const hours: number = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes: number = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds: number = Math.floor((difference % (1000 * 60)) / 1000);
-        return { days, hours, minutes, seconds };
-    }
+  constructor(name:string,age:number,rollno:number){
+      super(name,age)
+      this.rollno = rollno;
+  }
+  registerForCourse(course:Course):void{
+  this.courses.push(course)
+  course.AddStudent(this);
+  }
+  tostring(): string {
+      return` Student {name: ${this.name}, age: ${this.age}, rollno: ${this.rollno}, courses: [${this.courses.map(course => course.name).join(",")}}`
+  }
+}
 
-    function updateCountdown() {
-     const { days, hours, minutes, seconds } = calculateTimeRemaining(targetDate);
-        console.log(`${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds left`);
-    }
+class Instructor extends Person{
+  salary:number;
+  courses:Course[]=[];
 
-    setInterval(updateCountdown, 1000);
- }
+  constructor(name:string,age:number,salary:number){
+      super(name,age);
+      this.salary = salary;
+  }
+  assignCourse(course:Course):void{
+  this.courses.push(course);
+  course.setInstructor(this);
+  
+  }
+  tostring(): string {
+      return`Instructor {name: ${this.name}, age: ${this.age}, salary: ${this.salary}, courses: [${this.courses.map(course => course.name).join(",")}]}`
+  }
+}
 
-startCountdown();
+class Course{
+  id:number;
+  name:string;
+  student:Student[] = [];
+  instructors:Instructor | null = null;
+
+  constructor(id:number,name:string,){
+  this.id = id;
+  this.name = name;
+  }
+  AddStudent(student:Student):void{
+      this.student.push(student);
+  }
+  setInstructor(instructor: Instructor):void{ 
+      this.instructors = instructor;
+  }
+  tostring():string{
+      return `Course {id: ${this.id}, name: ${this.name}, student: [${this.student.map(student => student.name).join(",")}], instructor: ${this.instructors?.name?? "none"}}`
+  }
+}
+
+class Department{
+  name:string;
+  courses:Course[] = [];
+
+  constructor(name:string){
+  this.name = name;
+  }
+  AddCourse(course:Course):void{
+      this.courses.push(course)
+  }
+  tostring():string{
+      return `Department {name: ${this.name}, courses: [${this.courses.map(course => course.name).join(",")}]}`
+  }
+}
+
+const student1 = new Student("aisha",18,1000);
+const student2 = new Student("Ahmed",30,1001);
+const student3 = new Student("fatima",26,1002);
+
+const instructor1 = new Instructor("Hamzah",23,5000000);
+const instructor2 = new Instructor("Asharib",22,3000000);
+const instructor3 = new Instructor("Okasha",20,1000000);
+
+const course1 = new Course(1,"Artifical Intelligence");
+const course2 = new Course(2,"Metaverse");
+const course3 = new Course(3,"Blockchain");
+
+
+student1.registerForCourse(course1);
+student2.registerForCourse(course2);
+student3.registerForCourse(course3);
+
+instructor1.assignCourse(course1);
+instructor2.assignCourse(course2);
+instructor1.assignCourse(course3);
+
+const department1 = new Department("Department of Computer Science");
+const department2 = new Department("Department of Digital Media");
+const department3 = new Department("Department of Information System");
+
+department1.AddCourse(course1);
+department2.AddCourse(course2);
+department3.AddCourse(course3);
+
+console.log(department1.courses[0].tostring());
+console.log(instructor1.courses.map(course => course.tostring()));
+console.log(student1.courses[0].tostring());
+console.log(course3.student.map(student => student.tostring()));
+console.log(student1.courses.map(course => course.tostring()));
+
+console.log("All Instructors");
+console.log(instructor1.tostring());
+console.log(instructor2.tostring());
+console.log(instructor3.tostring());
+
+console.log("All Students");
+console.log(student1.tostring());
+console.log(student2.tostring());
+console.log(student3.tostring());
